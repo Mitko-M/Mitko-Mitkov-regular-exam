3aec15a67c4ab2387db381847ea3226c29b74a2e
Mitko Mitkov <mitkomitkov2727@gmail.com>
2024-06-30 14:45:05 +0300

    Deleted useless console log

--- tests/ui/about.test.js
+++ tests/ui/about.test.js
@@ -3,7 +3,6 @@ const { test, expect } = require('@playwright/test');
 test('Check about page', async ({ page }) => {
   await page.goto('http://localhost:8080/about');
   const heading = await page.$('h1');
-  console.log(heading);
   const text = await heading.textContent();
   expect(text).toBe('About');
 });
